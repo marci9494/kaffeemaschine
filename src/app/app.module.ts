fecb0fa474f2e6ed8095f8c4128967f03b7f97bc
Marcel Ament <marcel.ament@mercatis.com>
2018-08-09 14:18:22 +0200

    created rest Provider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,11 @@ import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+import { HttpClientModule } from "@angular/common/http";
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { RestProvider } from '../providers/rest/rest';
 
 @NgModule({
   declarations: [
@@ -21,6 +23,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
@@ -34,7 +37,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    RestProvider
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,30 +1,41 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-import { LoadingController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
+import {LoadingController} from 'ionic-angular';
+import {RestProvider} from '../../providers/rest/rest';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-  
+
 export class HomePage {
 
-  constructor(public navCtrl: NavController, public loadingCtrl: LoadingController) {
-   
+  status: any;
+  constructor(public navCtrl: NavController, public loadingCtrl: LoadingController, public restProvider: RestProvider) {
+
   }
-  order(art){
+  order(art) {
     console.log(art);
     this.presentLoading();
+    this.getStatus();
   }
-  
-   presentLoading() {
+
+  presentLoading() {
     const loader = this.loadingCtrl.create({
       content: "Please wait...",
       duration: 3000
     });
     loader.present();
   }
-  
+
+  getStatus() {
+    this.restProvider.getStatus()
+      .then(data => {
+        this.status = data;
+        console.log(this.status);
+      });
+  }
+
 
 
 }
