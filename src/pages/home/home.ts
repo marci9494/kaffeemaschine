0c21fe69b17971a9dbab9448b19f38bb8a59e8fe
Marcel Ament <marcel.ament@mercatis.com>
2018-08-09 11:29:21 +0200

    worked on home

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,35 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { LoadingController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+  
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, public loadingCtrl: LoadingController) {
+   
   }
+  order(art){
+    console.log(art);
+    this.presentLoading();
+  }
+  
+   presentLoading() {
+    const loader = this.loadingCtrl.create({
+      content: "Please wait...",
+      duration: 3000
+    });
+    loader.present();
+  }
+  
+
 
 }
+
+
+
+
+
